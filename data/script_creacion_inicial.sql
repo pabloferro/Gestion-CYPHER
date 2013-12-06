997a87e1b40942ba539e6a553c816eddae78a231
pabloferro <pablo.pyp@gmail.com>
2013-12-06 20:12:17 -0300

    Al top 5 le mando al fecha de hoy por parámetro

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -970,6 +970,28 @@ END
 GO
 --/MEDICOS
 
-COMMIT TRAN
+--ESTADISTICAS
+create function CIPHER.enElMismoAnoYMes(@fecha1 Datetime,@fecha2 Datetime)
+returns bit
+as
+begin
+if(YEAR(@fecha1)=YEAR(@fecha2) and MONTH(@fecha1)=MONTH(@fecha2))
+	return 1
 
---CONVERT(TIME,TURN_FECHA
+return 0
+end
+
+GO
+
+create procedure CIPHER.top5BonosFVencidos(@fecha DateTime, @HOY DATETIME)
+as
+begin
+select top 5 (select count(*) from CIPHER.COMPRA join CIPHER.BONOFARMACIA on BONF_COMPRA=COMP_CODIGO and COMP_AFILIADO=AFIL_NROAFILIADO and BONF_AFILIADOCONSUMIO is null where BONF_FECHAVENCIMIENTO>@HOY and CIPHER.enElMismoAnoYMes(BONF_FECHAVENCIMIENTO,@fecha)=1),
+AFIL_NOMBRE,AFIL_APELLIDO
+from CIPHER.AFILIADO 
+order by 1 desc
+end
+
+GO
+
+COMMIT TRAN
